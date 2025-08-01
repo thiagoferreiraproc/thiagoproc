3ae41d09ac6a9b75a16ebb2796c5ea03551ad72f
thiagoferreiraproc <thiagoferreiiraproc@gmail.com>
2025-08-01 15:49:07 -0300

    Delete src directory

--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Navbar() {
-  return (
-    <header className="flex items-center justify-between bg-procBlue p-4 text-white">
-      <h1 className="text-xl font-semibold">PROC Monitoring</h1>
-    </header>
-  );
-}
